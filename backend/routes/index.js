40e7a19835959a1c0eb6b49b1a562fba8c62df39
kartik-pant-23 <pantkartik23@gmail.com>
2021-01-04 13:27:55 +0530

    Added route for index page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const router = express.router
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const Group = require("../models/group")
+const Blogs = require("../models/blogs")
+
+router.get('/', (req, res) => {
+    var groupsDetails, blogDetails;
+    
+    // Find all the groups
+    Group.find()
+    .select('_id name group_score')
+    .exec()
+    .then((groups) => {
+        groupsDetails = groups;
+    })
+    .catch((error) => {
+        res.status(500).json({
+            message: error.message || "Something went wrong!"
+        })
+    })
+
+    // Find all the blogs
+    Blogs.find()
+    .exec()
+    .then((blogs) => {
+        blogDetails = blogs;
+    })
+    .catch((error) => {
+        res.status(500).json({
+            message: error.message || "Something went wrong!"
+        })
+    })
+
+    // Also need to fetch details of current discussion
+
+    // Everything found successfully, then return json
+    res.status(200).json({
+        groups: groupsDetails,
+        blogs: blogDetails
+    })
+
 });
 
 module.exports = router;
